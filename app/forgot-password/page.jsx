8af997f8703e15e2a6e3d9efe75a097f217af3da
Silvio <odeiroma@outlook.it>
2026-05-17 13:30:14 +0200

    update

--- app/forgot-password/page.jsx
+++ app/forgot-password/page.jsx
@@ -28,7 +28,7 @@ export default function ForgotPasswordPage() {
 
   // 🔥 Controllo se la mail esiste nella tabella users
   const { error } = await supabase.auth.resetPasswordForEmail(cleanEmail, {
-  redirectTo: `${window.location.origin}/reset-password`
+  redirectTo: "https://droneguard.it/reset-password"
 })
 
   if (error) {
