2a8fed116940aa01681ab2a7c2a17f6c6ac0d305
Silvio <odeiroma@outlook.it>
2026-05-19 19:01:22 +0200

    update

---
+++ app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://droneguard.it/sitemap.xml",
+  }
+}
---
+++ app/sitemap.js
@@ -0,0 +1,28 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://droneguard.it",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/come-funziona",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/faq",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/contatti",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/login",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/register",
+      lastModified: new Date(),
+    },
+  ]
+}
