ccd66043bf7bae0b1f9da01689e483af9e086b6c
Silvio <odeiroma@outlook.it>
2026-05-20 17:34:12 +0200

    update

--- app/sitemap.js
+++ app/sitemap.js
@@ -24,5 +24,21 @@ export default function sitemap() {
       url: "https://droneguard.it/register",
       lastModified: new Date(),
     },
+    {
+      url: "https://droneguard.it/ispezioni-con-drone",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/trova-pilota-drone",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/termografia-con-drone",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://droneguard.it/piloti-drone-certificati",
+      lastModified: new Date(),
+    }
   ]
 }
