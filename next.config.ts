f7103097de5341681d6d80b1d73855fd7944466a
Silvio <odeiroma@outlook.it>
2026-06-01 05:23:54 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,22 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(process.cwd()),
   },
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "droneguard.it",
+          },
+        ],
+        destination: "https://www.droneguard.it/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
